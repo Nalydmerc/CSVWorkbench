436c8ef6e8c7678e6089aeb8403fa3a7ff44f442
Intern <intern@3Birds.local>
2015-12-16 11:26:26 -0500

    Initial commit

---
+++ com/company/CSVUtils.java
@@ -0,0 +1,118 @@
+package com.company;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class CSVUtils {
+
+    public void writeCSV(String path, ArrayList<String[]> lines) {
+        System.out.println("Writing File: " + path + "...");
+        try {
+            PrintWriter writer = new PrintWriter(path);
+            for (String[] line : lines) {
+                String toWrite = "\"";
+                for (String val:line) {
+                    if (val == null) {
+                        val = "";
+                    }
+                    val = val.replace("\"", "\"\"");
+                    toWrite += val;
+                    toWrite += "\",\"";
+                }
+                toWrite = toWrite.substring(0, toWrite.length()-2);
+                writer.println(toWrite);
+            }
+            writer.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Done.");
+    }
+
+
+    /*  Read CSV file into an ArrayList<String[]>
+    Results are recorded as:
+    {value, value}
+    {value, value}, etc.
+    Such that every entry in the list is a line, where the line is an array containing it's columns.
+*/
+    public ArrayList<String[]> getCSV(String path) {
+        String fileName = path.substring(path.lastIndexOf("\\")+1);
+        System.out.println("Reading file: " + fileName + "...");
+        ArrayList<String[]> csvFile = new ArrayList<String[]>();
+        String csvPath = path;
+        BufferedReader br = null;
+        String line = "";
+
+        try {
+            BufferedReader readFirstList = new BufferedReader(new FileReader(csvPath));
+            String firstLine = readFirstList.readLine();
+            String[] separated = firstLine.split(",");
+            int numColumns = separated.length;
+
+            br = new BufferedReader(new FileReader(csvPath));
+            while ((line = br.readLine()) != null) {
+
+                ArrayList<String> columns = new ArrayList<>();
+                String value = "";
+                boolean inQuotes = false;
+
+                for (int i = 0; i < line.length(); i++) {
+                    char next = line.charAt(i);
+                    if (next == '"') {
+                        if (inQuotes) {
+
+                            boolean endVal = false;
+
+                            if (line.length() > i+1) {
+                                if (line.charAt(i+1) == '"') {
+                                    value += next;
+                                    i++;
+                                } else {
+                                    endVal = true;
+                                }
+                            } else {
+                                endVal = true;
+                            }
+
+                            if (endVal) {
+                                inQuotes = false;
+                                columns.add(value);
+                                value = "";
+                                i++;
+                            }
+
+                        } else {
+                            inQuotes = true;
+                        }
+                    } else if (next == ',') {
+                        if (!inQuotes) {
+                            columns.add(value);
+                            value = "";
+                        } else {
+                            value += next;
+                        }
+                    } else {
+                        value += next;
+                    }
+                }
+
+                csvFile.add(columns.toArray(new String[numColumns]));
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        System.out.println("Done.");
+        return csvFile;
+    }
+}
